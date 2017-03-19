8fd62e856416fa1f58c7eea71dd599d1c5d98e42
choniawko <pawel.choniawko@gmail.com>
2017-03-19 10:23:34 +0100

    task list service filter

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ITask } from './task/task.interface';
+import { TaskListService } from './task-list.service';
 
 @Component({
   selector: 'app-task-list',
@@ -9,18 +10,37 @@ import { ITask } from './task/task.interface';
 export class TaskListComponent implements OnInit {
 
   tasks: ITask[];
+  taskFilterList: string[];
+  selectedTaskFilter: string;
 
-  constructor() { }
+  constructor(private _taskListService: TaskListService) {
+    this.tasks = this._taskListService.tasks;
+    this.taskFilterList = ['wszystkie', 'otwarte', 'zamknięte'];
+    this.selectedTaskFilter = 'wszystkie';
+   }
 
-  ngOnInit() {
-    this.tasks = [
-      {title: 'Task #1', done: false},
-      {title: 'Task #2', done: false}
-    ];
+  ngOnInit() { }
+
+  get getFilteredTasks(): ITask[] {
+    return this._taskListService.tasks ?
+      this._taskListService.tasks.filter((task: ITask) => {
+        let result: boolean;
+        switch (this.selectedTaskFilter) {
+          case 'otwarte':
+            result = task.done;
+            break;
+          case 'zamknięte':
+            result = !task.done;
+            break;
+          default:
+            result = true;
+        }
+        return result;
+      }) : [];
   }
 
   addTask(title: string): void {
-    this.tasks.push({
+    this._taskListService.tasks.push({
       title,
       done: false
     });
--- src/app/task-list/task-list.service.ts
+++ src/app/task-list/task-list.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { ITask } from './task/task.interface';
 
 @Injectable()
 export class TaskListService {
 
-  constructor() { }
+  tasks: ITask[];
+  constructor() {
+    this.tasks = [
+      {title: 'Task #1', done: false},
+      {title: 'Task #2', done: false},
+      {title: 'Task #3', done: true},
+      {title: 'Task #4', done: false}
+    ];
+   }
+
+
 
 }
